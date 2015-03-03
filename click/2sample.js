50459a8a9011bc2ad71a94d4a38aecadc3017e52
shwalesrawesome <mhammond@connect.kellettschool.com>
2015-03-03 16:28:58 +0800

    Changed website template

--- click/2sample.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x;
-    if (confirm("Choose wisely") == true) {
-        x = "You pressed OK! woot woot go you";
-    } else {
-        x = "You pressed Cancel! meh...try ok";
-    }
-    document.getElementById("demo").innerHTML = x;
-}
